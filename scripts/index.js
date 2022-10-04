eec9ce4dd1221273de09c4625d5053246e2c83f3
Mwardentoumz <thomas.thivolet@gmail.com>
2022-10-05 00:53:54 +0200

    filters update + toggle
    filters update + toggle

--- scripts/index.js
+++ scripts/index.js
@@ -2,45 +2,43 @@ import { RecipeCard } from './RecipeFactory.js'
 
 class App {
     constructor() {
+        // retrieve the data
         this.RecipesApi = new RecipesApi('../assets/data/recipes.json')
 
     }
     async displayData() {
+        // retrieve the deck section to insert recipes cards
         const RecipesSection = document.getElementById("deck");
-        // console.log(RecipesSection)
-
+        // store promess of fetch request
         const RecipesData = await this.RecipesApi.getRecipes();
-        // console.log(RecipesData)
-
+        // map the card object with fetch data
         const NewRecipe = RecipesData.map(data => new RecipeCard(data))
-        // console.log(NewRecipe)
+        
 
-
+        // initiate empty arrays for later filters data
         const AllIng = []
         const AllUstensils = []
         const AllAppliance = []
 
+        // loop through our new objects to create cards
         NewRecipe.forEach(RecipeCard => {
             RecipeCard.$wrapperCard = document.createElement('div')
-
             const arr = RecipeCard.ingredients
+            // separate appliances into unique elements
+            let Appslice = RecipeCard._appliance.split(',')
+            // push elements into arrays for filters
+            AllUstensils.push(RecipeCard._ustensils)
+            AllAppliance.push(Appslice[0])
 
-
-
-
+            // this function generates a list for ingredients inside the recipe card
             function makeUL(arr) {
                 let list = document.createElement('ul')
                 arr.forEach(ingredient => {
-
                     AllIng.push(ingredient.ingredient.toLowerCase())
-
                     // on vérifie si la proprieté contient l'élement contient la proprieté unité & quantité - c'est l'équivalent de if/else
-
                     let unit = ingredient?.unit ? ingredient.unit : ""
                     let quantity = ingredient?.quantity ? ingredient.quantity : ""
-
                     // puis on renvoie la liste d'ingrédients dans le DOM
-
                     let li = document.createElement('li')
                     li.innerHTML = ingredient.ingredient + " : " + quantity + " " + unit
                     list.appendChild(li)
@@ -48,7 +46,7 @@ class App {
 
                 return list.innerHTML
             }
-
+            // this is the HTML for the cards
             var card = `
                                 <div class="card center mb-5" style="width: 32rem; max-height: 600px">
                                     <img class="card-img-top" src="../assets/data/grey.jpg" alt="Card image cap">
@@ -68,50 +66,71 @@ class App {
                                 </div>`
             RecipeCard.$wrapperCard.innerHTML = card
             RecipesSection.appendChild(RecipeCard.$wrapperCard)
-
-
-
-            // let set = new Set(AllIng)
-            // console.log(set)
         })
 
+        console.log(AllUstensils)
         // return a list of elements under the dropdown button for ingredients
         let IngDropdown = document.getElementById('ingrédients')
         let IngUL = document.createElement('ul')
         IngDropdown.appendChild(IngUL)
-        console.log(IngDropdown)
-        let set = new Set(AllIng)
-        // console.log(set)
-        set.forEach(element => {
-            let IngLi = document.createElement('li')
-            IngLi.innerHTML = element
+        let set1 = new Set(AllIng)
+        
+        set1.forEach(element => {
+            let IngLi = document.createElement('a')
+            IngLi.setAttribute('href', '#')
+            IngLi.classList = 'dropdown-item'
+            IngLi.innerHTML = " " + element
             IngUL.appendChild(IngLi)
-            // console.log(element)
+            
         })
-        // make the button dropdown display the list
+        // return a list of elements under the dropdown button for ustensils
+        let UstDropdown = document.getElementById('Ustensiles')
+        let UstUl = document.createElement('ul')
+        UstDropdown.appendChild(UstUl)
+        let set2 = new Set(AllUstensils)  
+        console.log(set2)
+        set2.forEach(element => {
 
+            let UstLi = document.createElement('a')
+            UstLi.setAttribute('href', '#')
+            UstLi.classList = 'dropdown-item'
+            UstLi.innerHTML = " " + element
+            UstUl.appendChild(UstLi)
+        })
+
+        // return a list of elements under the dropdown button for appliances
+        let AppDropdown = document.getElementById('Appliances')
+        let AppUl = document.createElement('ul')
+        AppDropdown.appendChild(AppUl)
+        let set3 = new Set(AllAppliance)
+        console.log(set3)
+        set3.forEach(element => {
+            
+            let AppLi = document.createElement('a')
+            AppLi.setAttribute('href', '#')
+            AppLi.classList = 'dropdown-item'
+            AppLi.innerHTML = " " + element
+            AppUl.appendChild(AppLi)
+        })
+
+        // toggle event for the filters buttons
+        
         document.addEventListener('click', (e) => {
-            var list = e.target.closest('button')
-            console.log(list)
-            var next = list.nextElementSibling
-            next.classList = 'show'
-            console.log(next)
-        })
-        // let dropdowns = document.getElementsByClassName("dropdown-menu");
-        // console.log(dropdowns)
-        // window.onclick = function (event) {
-        //     if (event.target.matches('dropdown')) {
-        //         event.target.nextSibling.classList.add('show');
 
-        //         console.log(dropdowns)
-        //         var i;
-        //         for (i = 0; i < dropdowns.length; i++) {
-        //             var openDropdown = dropdowns[i];
-        //             if (openDropdown.classList.contains('show')) {
-        //                 openDropdown.classList.remove('show');
-        //             }
-        //         }
-        //     }
+            
+            var list = e.target.closest('button')
+            if(list.nextElementSibling.classList.contains('show')){
+                list.nextElementSibling.classList.remove('show')
+                list.nextElementSibling.style.display= 'none'
+            }else{
+                list.nextElementSibling.classList.add('show')
+                list.nextElementSibling.style.display = 'inline-block'
+            }
+            
+        })
+
+
+       
     }
 
 
