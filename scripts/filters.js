059fb6d154014e036e752ebd826b80ed04e9a6ec
Mwardentoumz <thomas.thivolet@gmail.com>
2022-10-12 19:20:09 +0200

    general update
    general update

---
+++ scripts/filters.js
@@ -0,0 +1,98 @@
+
+
+// générer la liste des filtres
+export function listeIngrédients(element) {
+    let IngDropdown = document.getElementById('ingrédients')
+    let set1 = new Set(element)
+
+    set1.forEach(element => {
+        let IngLi = document.createElement('a')
+        IngLi.setAttribute('href', '#')
+        IngLi.classList = 'dropdown-item'
+        IngLi.innerHTML = " " + element
+        IngDropdown.appendChild(IngLi)
+
+    })
+}
+export function listeUstensiles(element) {
+    let UstDropdown = document.getElementById('Ustensiles')
+    let set2 = new Set(element)
+    
+    set2.forEach(element => {
+
+        let UstLi = document.createElement('a')
+        UstLi.setAttribute('href', '#')
+        UstLi.classList = 'dropdown-item'
+        UstLi.innerHTML = " " + element
+        UstDropdown.appendChild(UstLi)
+    })
+}
+export function listesAppareils(element) {
+    let AppDropdown = document.getElementById('Appliances')
+    let set3 = new Set(element)
+    set3.forEach(element => {
+
+        let AppLi = document.createElement('a')
+        AppLi.setAttribute('href', '#')
+        AppLi.classList = 'dropdown-item'
+        AppLi.innerHTML = " " + element
+        AppDropdown.appendChild(AppLi)
+    })
+}
+// gérer l'évenement toggle sur les listes déroulantes
+export function gestionClic() {
+    let filterClickEvent = document.getElementById('clickEvent')
+    filterClickEvent.addEventListener('click', (e) => {
+        var list = e.target.closest('button')
+        console.log(list.parentNode.parentNode)
+        var closeInputField = list.parentNode.parentNode
+        console.log(closeInputField)
+        if (list.nextElementSibling.classList.contains('show')) {
+            list.ariaExpanded = 'false'
+            closeInputField.classList.remove('expanded')
+            list.nextElementSibling.classList.remove('show')
+            list.nextElementSibling.style.display = 'none'
+        } else {
+            list.ariaExpanded = 'true'
+            closeInputField.classList.add('expanded')
+            list.nextElementSibling.classList.add('show')
+            list.nextElementSibling.style.display = 'inline-block'
+        }                                                                           
+
+    })
+}
+// eventListener sur un clic dans la liste des filtres
+export function clickonFilters() {
+    let clicktarget = document.getElementsByClassName('dropdown-item')
+    console.log(clicktarget)
+    for (let item of clicktarget) {
+
+        item.addEventListener('click', (e) => {
+            e.preventDefault()
+            console.log(e.target.innerHTML)
+            let btnValue = e.target.innerHTML
+            createButton(btnValue)
+        })
+    }
+}
+
+// créer le bouton lors du clic dans les filtres
+function createButton(btnValue) {
+    let newFilter = document.createElement('button')
+    newFilter.classList.add('btnfilters')
+    let closeIcon = ` <span class="material-symbols-outlined">cancel</span>`
+    newFilter.innerHTML = btnValue + closeIcon
+
+    console.log(newFilter)
+    let btnPlacement = document.getElementById('filtres-active')
+    btnPlacement.appendChild(newFilter)
+    removeFilter(newFilter)
+}
+
+// fonction pour retirer le filtre
+function removeFilter(newFilter) {
+    newFilter.addEventListener('click', (e) => {
+        e.preventDefault()
+        newFilter.remove()
+    })
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,7 @@
 import { RecipeCard } from './RecipeFactory.js'
+import { clickonFilters, gestionClic, listeIngrédients, listeUstensiles, listesAppareils } from './filters.js';
+
+
 
 // DOM ELEMENTS
 const mainsearch = document.getElementById('mainsearch')
@@ -19,41 +22,33 @@ class App {
     constructor() {
         // retrieve the data
         this.RecipesApi = new RecipesApi('../assets/data/recipes.json')
-        // retrieve the deck section to insert recipes cards
-
-        // store promess of fetch request
-
-
-        // map the card object with fetch data
-
     }
-
+    // cette fonction récupère l
     async getApi() {
         return await this.RecipesApi.getRecipes()
     }
     // pour afficher les éléments sans recherche et en cas de retour à pas de recherche
     async displayData(element) {
-        // clear deck inner HTML
-        
-        // initiate empty arrays for later filters data
-        let set1 = []
-        let set2 = []
-        let set3 = []
-        
         const NewRecipe = element.map(data => new RecipeCard(data))
-
-        // loop through our new objects to create cards
+        // boucler dans les élements récuperés pour créer les cartes
         NewRecipe.forEach(element => {
             element.$wrapperCard = document.createElement('div')
             const arr = element.ingredients
             // separate appliances into unique elements
             let Appslice = element._appliance.split(',')
+            console.log(element._ustensils)
+            for (let i = 0; i < element._ustensils.length; i++){
+                AllUstensils.push(element._ustensils[i])
+            }
+
+
             // push elements into arrays for filters
-            AllUstensils.push(element._ustensils)
+
+
             AllAppliance.push(Appslice[0])
-            // this function generates a list for ingredients inside the recipe card
+            // générer la liste d'ingrédients
             this.makeUL(arr)
-            // this is the HTML for the cards
+            // contenu HTML des cartes
             var card = `
                     <div class="card center mb-5" style="width: 32rem; max-height: 600px">
                         <img class="card-img-top" src="../assets/data/grey.jpg" alt="Card image cap">
@@ -71,71 +66,20 @@ class App {
                             </div>
                         </div>
                     </div>`
-                    element.$wrapperCard.innerHTML = card
-
+            element.$wrapperCard.innerHTML = card
             RecipesSection.appendChild(element.$wrapperCard)
+            // récupérer les listes pour les filtres
+            
+            // récupérer l'évenement du clic pour ouvrir les listes
+            
         })
-        // cette fonction reprends les ingrédients lorsque la boucle est initiée et génère la liste des ingrédients
-
-        // return a list of elements under the dropdown button for ingredients
-        let IngDropdown = document.getElementById('ingrédients')
-        let IngUL = document.createElement('ul')
-        IngDropdown.appendChild(IngUL)
-        set1 = new Set(AllIng)
-
-        set1.forEach(element => {
-            let IngLi = document.createElement('a')
-            IngLi.setAttribute('href', '#')
-            IngLi.classList = 'dropdown-item'
-            IngLi.innerHTML = " " + element
-            IngUL.appendChild(IngLi)
-
-        })
-        // return a list of elements under the dropdown button for ustensils
-        let UstDropdown = document.getElementById('Ustensiles')
-        let UstUl = document.createElement('ul')
-        UstDropdown.appendChild(UstUl)
-        set2 = new Set(AllUstensils)
-        set2.forEach(element => {
-
-            let UstLi = document.createElement('a')
-            UstLi.setAttribute('href', '#')
-            UstLi.classList = 'dropdown-item'
-            UstLi.innerHTML = " " + element
-            UstUl.appendChild(UstLi)
-        })
-
-        // return a list of elements under the dropdown button for appliances
-        let AppDropdown = document.getElementById('Appliances')
-        let AppUl = document.createElement('ul')
-        AppDropdown.appendChild(AppUl)
-        set3 = new Set(AllAppliance)
-        set3.forEach(element => {
-
-            let AppLi = document.createElement('a')
-            AppLi.setAttribute('href', '#')
-            AppLi.classList = 'dropdown-item'
-            AppLi.innerHTML = " " + element
-            AppUl.appendChild(AppLi)
-        })
-
-        // toggle event for the filters buttons
-        let filterClickEvent = document.getElementById('clickEvent')
-        filterClickEvent.addEventListener('click', (e) => {
-
-
-            var list = e.target.closest('button')
-            if (list.nextElementSibling.classList.contains('show')) {
-                list.nextElementSibling.classList.remove('show')
-                list.nextElementSibling.style.display = 'none'
-            } else {
-                list.nextElementSibling.classList.add('show')
-                list.nextElementSibling.style.display = 'inline-block'
-            }
-
-        })
+        listeIngrédients(AllIng)
+        listeUstensiles(AllUstensils)
+        listesAppareils(AllAppliance)
+        console.log(AllUstensils)
 
     }
+    // cette fonction gère la liste des ingrédients dans chaque carte
     makeUL(arr) {
         let list = document.createElement('ul')
         arr.forEach(ingredient => {
@@ -148,9 +92,9 @@ class App {
             li.innerHTML = ingredient.ingredient + " : " + quantity + " " + unit
             list.appendChild(li)
         })
-
         return list.innerHTML
     }
+
     // cette fonction écoute le champ de recherche principal
     listenInput(RecipesData) {
         console.log(concat)
@@ -164,13 +108,13 @@ class App {
                 this.findInDescription(inputValue, RecipesData)
                 this.concatener(resultsTitles, resultsDescriptions, resultsIngredients)
                 this.displayData(concat)
-            } else if(inputValue.length < 3){
+            } else if (inputValue.length < 3) {
                 RecipesSection.innerHTML = ""
                 this.displayData(RecipesData)
             }
         })
     }
-    
+
     // cette fonction recherche le résultat de l'input dans les titres des recettes
     findInTitle(inputValue, RecipesData) {
         resultsTitles = []
@@ -179,54 +123,48 @@ class App {
                 resultsTitles.push(RecipesData[i])
             }
         }
-        
+
     }
+
     // cette fonction recherche le résultat de l'input dans les descriptions des recettes
-    findInDescription(inputValue, RecipesData){
+    findInDescription(inputValue, RecipesData) {
         resultsDescriptions = []
-        for (let i=0; i < RecipesData.length; i++){
-            if (RecipesData[i].description.toLowerCase().includes(inputValue)){
+        for (let i = 0; i < RecipesData.length; i++) {
+            if (RecipesData[i].description.toLowerCase().includes(inputValue)) {
                 resultsDescriptions.push(RecipesData[i])
             }
         }
     }
+
     // cette fonction recherche le résultat de l'input dans les ingrédients des recettes
-    findInIngredients(inputValue, RecipesData){
+    findInIngredients(inputValue, RecipesData) {
         resultsIngredients = []
-        for (let i=0; i < RecipesData[i].length; i++){
-            if(RecipesData[i].ingredients.toLowerCase().includes(inputValue)){
+        for (let i = 0; i < RecipesData[i].length; i++) {
+            if (RecipesData[i].ingredients.toLowerCase().includes(inputValue)) {
                 resultsIngredients.push(RecipesData[i])
             }
         }
     }
+
     // concatener les 3 résultats ensembles
-    concatener(resultsTitles, resultsDescriptions, resultsIngredients){
+    concatener(resultsTitles, resultsDescriptions, resultsIngredients) {
         resultsConcat = resultsTitles.concat(resultsDescriptions, resultsIngredients)
         // console.log(resultsConcat)
         concat = Array.from(new Set(resultsConcat))
         console.log(concat)
         let element = concat.map(data => new RecipeCard(data))
         console.log(element)
-        
-        
-        
     }
-    // retirer les cartes avant de boucler pour afficher le résultat de recherche
-    removeCards(){
-        let cards = document.getElementsByClassName('card center mb-5')
-        for(let i=0; i <cards.length; i++){
-            const card = cards[i]
-            card.remove()
-        }
-    }
-    async init() {
 
+    async init() {
         // const RecipesData = await this.getApi()
         let RecipesData = await this.getApi()
         this.displayData(RecipesData)
         this.listenInput(RecipesData)
+        clickonFilters()
+        gestionClic()
     }
-    
+
 }
 
 const app = new App()
